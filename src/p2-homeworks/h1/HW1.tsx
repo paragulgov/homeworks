6f7c575330d02599d846f20748f50cdc742bbe3a
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-02-06 02:44:21 +0300

    hw1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Message from './Message'
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    name: 'Artem',
+    message: 'npm start нажимал? если нет, то нажми. Здесьоченьдлинноесообщениекотороепереноситсялибоудлинняетблок. Спасибо за внимание! Все пока и сладких снов.',
+    time: new Date().toLocaleTimeString(),
 }
 
 function HW1() {
@@ -15,12 +16,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import classes from './Message.module.css'
 
-function Message() {
+type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
-        <div>
+        <div className={classes.message}>
+
+            <div className={classes.messageWrapper}>
+
+                <div className={classes.avatar}>
+                    <img src={props.avatar} alt="avatar" />
+                </div>
+
+                <div className={classes.text}>
+                    <span className={classes.name}>{props.name}</span>
+                    <div className={classes.messageText}>
+                        <div className={classes.ug} />
+                        {props.message}
+                    </div>
+                    <span className={classes.time}>{props.time}</span>
+                </div>
+
+            </div>
 
         </div>
     )
