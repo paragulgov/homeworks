2cba306291f7a7ca77a3f9ebd04ac43eca8b7bfc
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-04-29 02:35:29 +0300

    hw7

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HW6 from '../../h6/HW6'
+import HW7 from '../../h7/HW7'
 
 
 function Junior() {
@@ -7,7 +8,7 @@ function Junior() {
         <div>
 
             <HW6 />
-            {/*<HW7/>*/}
+            <HW7/>
             {/*<HW8/>*/}
             {/*<HW9/>*/}
             {/*<HW10/>*/}
